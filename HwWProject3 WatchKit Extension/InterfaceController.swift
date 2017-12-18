2da312fc45f8f4bfb8c49f7b14126148cc82d7fe
Skyler Svendsen <skylersvendsen@gmail.com>
2017-12-17 20:14:14 -0600

    first commit

--- HwWProject3 WatchKit Extension/InterfaceController.swift
+++ HwWProject3 WatchKit Extension/InterfaceController.swift
@@ -15,7 +15,55 @@ class InterfaceController: WKInterfaceController {
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
-        // Configure interface objects here.
+        testUserDefaults()
+        testUserDefaults()
+        
+        testKeychain()
+        testKeychain()
+        
+        testFile()
+        testFile()
+    }
+    
+    func testUserDefaults() {
+        let defaults = UserDefaults.standard
+        
+        if let contents = defaults.string(forKey: "shared_default") {
+            print("Reading from defaults")
+            print(contents)
+        } else {
+            print("Writing to defaults")
+            defaults.set("This is the saved default.", forKey: "shared_default")
+        }
+    }
+    
+    func testKeychain() {
+        let keychain = KeychainWrapper.standard
+        
+        if let contents = keychain.string(forKey: "shared_keychain") {
+            print("Reading from keychain")
+            print(contents)
+        } else {
+            print("Writing to keychain")
+            keychain.set("This is the saved keychain.", forKey: "shared_keychain")
+        }
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+    
+    func testFile() {
+        let saveURL = getDocumentsDirectory().appendingPathComponent("shared_file")
+        
+        if let contents = try? String(contentsOf: saveURL) {
+            print("Reading from file")
+            print(contents)
+        } else {
+            print("Writing to file")
+            try? "This is the saved file".write(to: saveURL, atomically: true, encoding: String.Encoding.utf8)
+        }
     }
     
     override func willActivate() {
